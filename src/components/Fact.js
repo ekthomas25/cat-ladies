4c4d466252df7cd636f8c5983fa786248240a1a0
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-28 16:36:08 -0800

    Add file to project
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-Authored-by: Kim Brannian <brannian@gmail.com>

---
+++ src/components/Fact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Fact extends React.Component {
+
+  render() {
+
+  }
+}
+
+export default Fact;
---
+++ src/components/Image.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Image extends React.Component {
+
+  render() {
+
+  }
+}
+
+export default Image;
