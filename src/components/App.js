3ab13b94df233a6c3ce3d714635a27e65cd4d65a
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-02-28 16:37:31 -0800

    Add file to project with components.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-Authored-by: Kim Brannian <brannian@gmail.com>

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header';
+import Fact from './Fact';
+import Image from './Image';
+
+function App() {
+  return (
+    <React.Fragment>
+      <Header />
+      <Fact />
+      <Image />
+    </React.Fragment>
+
+  );
+}
+
+export default App;
