68f449241008c50a21746a35f9282f551baff410
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-02 14:06:01 -0800

    Add hooks and images.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-Authored-by: Kim Brannian <brannian@gmail.com>

--- src/components/App.js
+++ src/components/App.js
@@ -4,17 +4,35 @@ import Header from './Header';
 import Fact from './Fact';
 import Image from './Image';
 import { useState } from 'react';
+import bowtieCat from '../images/christy-bowtie-cat.png';
+import indyTeeth from '../images/IndyTeeth.png';
+import katieCat from '../images/katie-cat.png';
+import kimCat from '../images/kim-cat.png';
 
 const queryClient = new QueryClient();
 
 function App() {
   const [page, setPage] = useState('page not set')
 
+  const onImageClick = () => {
+    setPage('image')
+  }
+
+  const onGifClick = () => {
+    setPage('gif')
+  }
+
   return (
     <QueryClientProvider client={queryClient}>
-      <Header />
-      <Fact />
-      <Image page={page} setPage={setPage}/>
+      <img className="cat-img" id="bowtieCat" src={bowtieCat}/>
+      <img className="cat-img" id="teethCat" src={indyTeeth}/>
+      <img className="cat-img" id="katieCat" src={katieCat}/>
+      <img className="cat-img" id="kimCat" src={kimCat}/>
+      <div className='container'>
+        <Header />
+        <Fact className='fact'/>
+        <Image page={page} onImageClick={onImageClick} onGifClick={onGifClick}/>
+      </div>
     </QueryClientProvider>
   );
 }
