d8a27d9433025c97396ae72b97f1dc85cb446db6
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-01 16:24:40 -0800

    Add API, useQuery WIP.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-Authored-by: Kim Brannian <brannian@gmail.com>

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-class Image extends React.Component {
+const fetchImage = async () => {
+  const res = await fetch('https://api.thecatapi.com/v1/images/search?mime_types=jpg,png');
+  return res.json();
+}
 
-  render() {
+const fetchGif = async () => {
+  const resGif = await fetch('https://api.thecatapi.com/v1/images/search?mime_types=gif');
+  return resGif.json();
+}
 
+const Image = (props) => {
+  const {data, status, refetch} = useQuery('image', fetchImage);
+  const gifResult = useQuery('gif', fetchGif);
+  const gifData = gifResult.data
+  const gifStatus = gifResult.status
+  const gifRefetch = gifResult.refetch 
+
+  console.log(gifResult)
+  console.log(props.page)
+
+  const onImageClick = () => {
+    refetch()
   }
+
+  const onGifClick = () => {
+    gifRefetch()
+  }
+
+  let currentStatus;
+  let currentData;
+
+  if (props.page === 'image') {
+    currentStatus = status
+    currentData = data
+  } else {
+    currentStatus = gifStatus
+    currentData = gifData
+  }
+
+  return (
+    <div>
+      {currentStatus === 'loading' && (
+        <div>Loading Cat Pictures</div>
+      )}
+
+      {currentStatus === 'error' && (
+        <div>Error fetching cats</div>
+      )}
+
+      {currentStatus === 'success' && (
+        <img src={`${currentData[0].url}`} />
+      )}
+
+      <button onClick={() => props.setPage('image'), onImageClick}>Random Cat Image</button>
+      <button onClick={() => props.setPage('gif'), onGifClick}>Random Cat Gif</button>
+    </div>
+  )
 }
 
 export default Image;
