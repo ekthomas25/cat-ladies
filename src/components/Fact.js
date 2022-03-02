75d5f59748108f58f1afecb388085232038272e0
Liz Thomas <thomas.elizabeth.k@gmail.com>
2022-03-01 16:24:06 -0800

    Add API, useQuery.
    Co-authored-by: Christy Welch <christinamawel@gmail.com>
    Co-authored-by: Katie Pundt <kzpundt@gmail.com>
    Co-Authored-by: Kim Brannian <brannian@gmail.com>

--- src/components/Fact.js
+++ src/components/Fact.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { useState } from 'react';
+import { useQuery } from 'react-query';
 
-class Fact extends React.Component {
+const fetchFact = async () => {
+  const res = await fetch(`https://cat-fact.herokuapp.com/facts/random?animal_type=cat&amount=1`);
+  return res.json();
+}
 
-  render() {
+const Fact = () => {
+  const { data, status } = useQuery('fact', fetchFact);
+  console.log(data);
+  return (
+    <div>
+      <h2>Cat Fact:</h2>
+      {status === 'loading' && (
+        <div>Loading...</div>
+      )}
 
-  }
+      {status === 'error' && (
+        <div>Error fetching data</div>
+      )}
+
+      {status === 'success' && (
+        <div>{data.text}</div>
+      )}
+    </div>
+  )
 }
 
 export default Fact;
